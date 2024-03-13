4dea7e44a30ae0bd29127787e048a6b4553c1538
Authapon Kongwan <authapon@gmail.com>
2024-03-13 14:56:18 +0700

    start

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ruts48code/random4ruts
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,29 @@
+package random4ruts
+
+import (
+	"crypto/rand"
+	"math/big"
+)
+
+func GetRandomInt(n int64) int64 {
+	nBig, _ := rand.Int(rand.Reader, big.NewInt(n))
+	return nBig.Int64()
+}
+
+func GetRandomBytes(n int64) []byte {
+	output := make([]byte, n)
+	_, err := rand.Read(output)
+	if err != nil {
+		return make([]byte, 0)
+	}
+	return output
+}
+
+func GetRandomString(n int) string {
+	salt := "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	output := ""
+	for i := 0; i < n; i++ {
+		output = output + string(salt[GetRandomInt(int64(len(salt)))])
+	}
+	return output
+}
